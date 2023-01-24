851271395b8269bb0ee46df783590fec8508b3a2
Mario David Riguera Castillo <mario.riguera@gmail.com>
2023-01-24 16:21:44 +0100

    Agregar archivos de proyecto.

---
+++ Sincronia/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Sincronia
+{
+    public class Program
+    {
+        /// <summary>
+        /// Inicio del programa.
+        /// </summary>
+        /// <param name="args">Argumentos.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
