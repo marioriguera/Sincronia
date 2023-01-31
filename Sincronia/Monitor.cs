fba1309e1f9f22aff80f3bfd6422623d15e477d1
Mario David Riguera Castillo <mario.riguera@gmail.com>
2023-01-31 17:20:56 +0100

    Iniciando pruebas con Monitor.

---
+++ Sincronia/Monitor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sincronia
+{
+    /// <summary>
+    /// Clase para el estudio de <see cref="System.Threading.Monitor" ./>.
+    /// </summary>
+    public class Monitor
+    {
+        /// <summary>
+        /// Listado utilizado para gestionarlo por threads.
+        /// </summary>
+        private static List<string> ListLoks = new List<string>() { "uno", "dos", "tres", "cuatro", "cinco" };
+
+        /// <summary>
+        /// Nuevo listado utilizado para agregar numeros.
+        /// </summary>
+        public static List<string> NuevosElementos { get; set; } = new List<string>() { "seis", "siete", "ocho" };
+
+        /// <summary>
+        /// Principal trabajo con hilos.
+        /// </summary>
+        public static void TrabajoHilos()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Imprimir).Start();
+                new Thread(AgregarNuevosElementos).Start();
+            }
+        }
+
+        /// <summary>
+        /// Impresión de elementos del listado en consola.
+        /// </summary>
+        private static void Imprimir()
+        {
+            Console.WriteLine($"Inicio de impresión. Id hilo: {Thread.CurrentThread.ManagedThreadId} .");
+            System.Threading.Monitor.Enter(ListLoks);
+            foreach (string s in ListLoks)
+            {
+                Console.WriteLine($" El numero a imprimir es: {s}");
+            }
+            Thread.Sleep(1000);
+            System.Threading.Monitor.Exit(ListLoks);
+            Console.WriteLine($"Fin de impresión. Id hilo: {Thread.CurrentThread.ManagedThreadId} .");
+        }
+
+        /// <summary>
+        /// Agregar nuevos elementos a <see cref="ListLoks" ./>
+        /// </summary>
+        private static void AgregarNuevosElementos()
+        {
+            Console.WriteLine($"Inicio de agregar elementos. Id hilo: {Thread.CurrentThread.ManagedThreadId} .");
+            System.Threading.Monitor.Enter(ListLoks);
+            foreach (string s in NuevosElementos)
+            {
+                ListLoks.Add(s);
+            }
+            Thread.Sleep(100);
+            System.Threading.Monitor.Exit(ListLoks);
+            Console.WriteLine($"Fin de agregar elementos. Id hilo: {Thread.CurrentThread.ManagedThreadId} .");
+        }
+    }
+}
--- Sincronia/Program.cs
+++ Sincronia/Program.cs
@@ -11,10 +11,19 @@
         /// <param name="args">Argumentos.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Pruebas de sincronía.");
+            Console.WriteLine("Pruebas de sincronía.\n");
 
             // Trabajos con lock.
+            Console.WriteLine("Trabajos con lock.\n");
             Lock.TrabajoHilos();
+            Thread.Sleep(10000);
+
+            Console.WriteLine("\n");
+
+            // Trabajos con monitor.
+            Console.WriteLine("Trabajos con monitor.\n");
+            Monitor.TrabajoHilos();
+            Thread.Sleep(10000);
 
             Console.ReadKey();
         }
